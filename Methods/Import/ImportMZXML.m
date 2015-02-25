4c8386837ede2aebaed378e6fc577326e76422b5
James Dillon <jamesdillonii@gmail.com>
2015-02-25 18:11:32 -0500

    Added features for '.mzXML' data conversion. Read scan information, MS level, peak list. Import time values and total intensity values.

--- Methods/Import/ImportMZXML.m
+++ Methods/Import/ImportMZXML.m
@@ -21,25 +21,27 @@
 
 function varargout = ImportMZXML(varargin)
 
-% WARNING: UNFINISHED METHOD 
-disp('UNFINISHED METHOD: CANNOT PROCEED')
-varargout{1} = [];
-return
-
 warning off all
 
 % Check input
-[file, data, options] = parse(varargin);
+[mzxml, data] = parse(varargin);
 
 % Check file name
-if isempty(file)
+if isempty(mzxml)
     varargout{1} = [];
     disp('Error: Input file invalid.');
     return
 end
 
+% Anonymous functions
+file.key = @(x,y,i) x.item(i).getElementsByTagName(y);
+file.value = @(x,y,i) char(x.item(i).getAttribute(y));
+
+file.element = @(x,y) x.getElementsByTagName(y);
+file.attribute = @(x,y) char(x.getAttribute(y));
+
 % Open document
-document = xmlread(file);
+document = xmlread(mzxml);
 
 % Read document elements
 file.mzxml = document.getDocumentElement;
@@ -53,10 +55,16 @@ if isempty(file.version)
     return
 end
 
-% Information fields
+% Information
 [file, data] = FileInfo(file, data);
 [file, data] = InstrumentInfo(file, data);
 [file, data] = ProcessingInfo(file, data);
+
+% Indexing
+[file, data] = ScanInfo(file, data);
+
+% Data
+[~, data] = ReadTIC(file, data);
 
 varargout{1} = data;
 end
@@ -71,8 +79,10 @@ function [file, options] = FileHeader(file, options)
 url = 'http://sashimi.sourceforge.net/schema_revision/mzXML_';
 
 % mzXML Version
-switch char(file.mzxml.getAttribute('xmlns'))
+switch file.attribute(file.mzxml, 'xmlns')
     
+    case [url, '1.1.1']
+        file.version = 1.1;
     case [url, '2.0']
         file.version = 2.0;
     case [url, '2.1']
@@ -91,11 +101,11 @@ switch char(file.mzxml.getAttribute('xmlns'))
         return
 end
 
-% Read mandatory fields
-file.msRun = file.mzxml.getElementsByTagName('msRun').item(0);
-file.index = file.mzxml.getElementsByTagName('index').item(0);
-file.indexOffset = file.mzxml.getElementsByTagName('indexOffset').item(0);
-file.sha1 = file.mzxml.getElementsByTagName('sha1').item(0);
+% Read primary fields
+file.msRun = file.element(file.mzxml, 'msRun');
+file.index = file.element(file.mzxml, 'index');
+file.indexOffset = file.element(file.mzxml, 'indexOffset');
+file.sha1 = file.element(file.mzxml, 'sha1');
 end
 
 
@@ -104,20 +114,22 @@ end
 % 
 function [file, data] = FileInfo(file, data)
 
+% Anonymous functions
+key = file.key;
+value = file.value;
+
 % /msRun/parentFile
-parentFile = file.msRun.getElementsByTagName('parentFile');
+parentFile = key(file.msRun, 'parentFile', 0);
 
 if parentFile.getLength > 0
-    file.parentFile = parentFile.item(0);
-else
-    return
+
+    % /msRun/parentFile/fileName
+    data.file.name = value(parentFile, 'fileName', 0);
+
+    % /msRun/parentFile/fileType
+    data.file.type = value(parentFile, 'fileType', 0);
+
 end
-
-% /msRun/parentFile/fileName
-data.file.name = char(file.parentFile.getAttribute('fileName'));
-
-% /msRun/parentFile/fileType
-data.file.type = char(file.parentFile.getAttribute('fileType'));
 end
 
 
@@ -126,75 +138,75 @@ end
 %
 function [file, data] = InstrumentInfo(file, data)
 
+% Anonymous functions
+key = file.key;
+value = file.value;
+
 % /msRun/msInstrument
-msInstrument = file.msRun.getElementsByTagName('msInstrument');
+msInstrument = key(file.msRun, 'msInstrument', 0);
 
 if msInstrument.getLength > 0
-    file.msInstrument = msInstrument.item(0);
-else
-    return
-end
 
-% /msRun/msInstrument/msManufacturer
-msManufacturer = file.msInstrument.getElementsByTagName('msManufacturer');
-
-if msManufacturer.getLength > 0
-    file.msManufacturer = msManufacturer.item(0);
-    data.instrument.vendor = char(file.msManufacturer.getAttribute('value'));
-end
-
-% /msRun/msInstrument/msModel
-msModel = file.msInstrument.getElementsByTagName('msModel');
-
-if msModel.getLength > 0
-    file.msModel = msModel.item(0);
-    data.instrument.model = char(file.msModel.getAttribute('value'));
-end
-
-% /msRun/msInstrument/msIonisation
-msIonisation = file.msInstrument.getElementsByTagName('msIonisation');
-
-if msIonisation.getLength > 0
-    file.msIonisation = msIonisation.item(0);
-    data.instrument.ionisation = char(file.msIonisation.getAttribute('value'));
-end
-
-% /msRun/msInstrument/msMassAnalyzer
-msMassAnalyzer = file.msInstrument.getElementsByTagName('msMassAnalyzer');
-
-if msMassAnalyzer.getLength > 0
-    file.msMassAnalyzer = msMassAnalyzer.item(0);
-    data.instrument.analyzer = char(file.msMassAnalyzer.getAttribute('value'));
-end
-
-% /msRun/msInstrument/msDetector
-msDetector = file.msInstrument.getElementsByTagName('msDetector');
-
-if msMassAnalyzer.getLength > 0
-    file.msDetector = msDetector.item(0);
-    data.instrument.detector = char(file.msDetector.getAttribute('value'));
-end
-
-% /msRun/msInstrument/msResolution
-msResolution = file.msInstrument.getElementsByTagName('msResolution');
-
-if msResolution.getLength > 0
-    file.msResolution = msResolution.item(0);
-    data.instrument.resolution = char(file.msResolution.getAttribute('value'));
-end
-
-% /msRun/msInstrument/software
-software = file.msInstrument.getElementsByTagName('software');
-software_version = file.msInstrument.getElementsByTagName('software');
-
-if software.getLength > 0
-    file.software = software.item(0);
-    data.instrument.software = char(file.software.getAttribute('name'));
-end    
-
-if software_version.getLength > 0
-    file.software_version = software_version.item(0);
-    data.instrument.software_version = char(file.software_version.getAttribute('version'));
+    % /msRun/msInstrument/msManufacturer
+    msManufacturer = key(msInstrument, 'msManufacturer', 0);
+    
+    if msManufacturer.getLength > 0
+        data.instrument.vendor = value(msManufacturer, 'value', 0);
+    end
+    
+    % /msRun/msInstrument/msModel
+    msModel = key(msInstrument, 'msModel', 0);
+    
+    if msModel.getLength > 0
+        data.instrument.model = value(msModel, 'value', 0);
+    end
+    
+    % /msRun/msInstrument/msIonisation
+    msIonisation = key(msInstrument, 'msIonisation', 0);
+    
+    if msIonisation.getLength > 0
+        data.instrument.ionisation = value(msIonisation, 'value', 0);
+    end
+    
+    % /msRun/msInstrument/msMassAnalyzer
+    msMassAnalyzer = key(msInstrument, 'msMassAnalyzer', 0);
+    
+    if msMassAnalyzer.getLength > 0
+        data.instrument.analyzer = value(msMassAnalyzer, 'value', 0);
+    end
+    
+    % /msRun/msInstrument/msDetector
+    msDetector = key(msInstrument, 'msDetector', 0);
+    
+    if msMassAnalyzer.getLength > 0
+        data.instrument.detector = value(msDetector, 'value', 0);
+    end
+    
+    % /msRun/msInstrument/msResolution
+    msResolution = key(msInstrument, 'msResolution', 0);
+    
+    if msResolution.getLength > 0
+        data.instrument.resolution = value(msResolution, 'value', 0);
+    end
+    
+    % /msRun/msInstrument/software
+    software = key(msInstrument, 'software', 0);
+    
+    if software.getLength > 0
+        data.instrument.software = value(software, 'name', 0);
+        data.instrument.software_version = value(software, 'version', 0);
+    end
+    
+    % /msRun/msInstrument/operator
+    operator = key(msInstrument, 'operator', 0);
+    
+    if operator.getLength > 0
+        %data.instrument.operator_first = value(operator, 'first', 0);
+        data.instrument.operator_last = value(operator, 'last', 0);
+        %data.instrument.operator_phone = value(operator, 'phone', 0);
+        %data.instrument.operator_email = value(operator, 'email', 0);
+        %data.instrument.operator_URI = value(operator, 'URI', 0);
+    end
 end
 end
 
@@ -204,15 +216,140 @@ end
 %
 function [file, data] = ProcessingInfo(file, data)
 
+% Anonymous functions
+key = file.key;
+value = file.value;
+
 % /msRun/dataProcessing
-dataProcessing = file.msRun.getElementsByTagName('dataProcessing');
+dataProcessing = key(file.msRun, 'dataProcessing', 0);
 
 if dataProcessing.getLength > 0
-    file.dataProcessing = dataProcessing.item(0);
-else
-    return
+    
+    % /msRun/dataProcessing/intensityCutoff
+    cutoff = value(dataProcessing, 'intensityCutoff', 0);
+    
+    if ~isempty(cutoff)
+        data.processing.cutoff = cutoff;
+    end
+    
+    % /msRun/dataProcessing/centroided
+    centroided = value(dataProcessing, 'centroided', 0);
+    
+    if ~isempty(centroided)
+        data.processing.centroided = centroided;
+    end
+    
+    % /msRun/dataProcessing/deisotoped
+    deisotoped = value(dataProcessing, 'deisotoped', 0);
+    
+    if ~isempty(deisotoped)
+        data.processing.deisotoped = deisotoped;
+    end
+    
+    % /msRun/dataProcessing/chargeDeconvoluted
+    chargeDeconvoluted = value(dataProcessing, 'chargeDeconvoluted', 0);
+    
+    if ~isempty(chargeDeconvoluted)
+        data.processing.charge_deconvoluted = chargeDeconvoluted;
+    end
+    
+    % /msRun/dataProcessing/spotIntegration
+    spotIntegration = value(dataProcessing, 'spotIntegration', 0);
+    
+    if ~isempty(spotIntegration)
+        data.processing.spot_integration = spotIntegration;
+    end
+    
+    % /msRun/dataProcessing/software
+    software = key(dataProcessing, 'software', 0);
+    
+    if software.getLength > 0
+        data.processing.type = value(software, 'type', 0);
+        data.processing.name = value(software, 'name', 0);
+        data.processing.version = value(software, 'version', 0);
+    end 
+end
 end
 
+
+%
+% Scan Information
+% 
+function [file, data] = ScanInfo(file, data)
+
+% Anonymous functions
+value = file.value;
+element = file.element;
+
+% /msRun/scan
+file.scan = element(file.mzxml, 'scan');
+
+if file.scan.getLength > 0
+    
+    % Display number of scans
+    disp(['Reading ', file.scan.getLength, ' scans']);
+    
+    % Index scans
+    for i = 1:file.scan.getLength
+        
+        % /msRun/scan/num
+        data.index(i).scan = str2double(value(file.scan, 'num', i-1));
+        
+        % /msRun/scan/msLevel
+        data.index(i).level = str2double(value(file.scan, 'msLevel', i-1));
+        
+        % /msRun/scan/peaksCount
+        data.index(i).peaks = str2double(value(file.scan, 'peaksCount', i-1));
+    end
+end
+end
+
+
+%
+% Total Intensity Values
+%
+function [file, data] = ReadTIC(file, data)
+
+% Anonymous functions
+value = file.value;
+
+% Pre-allocate memory
+data.time{file.scan.getLength} = [];
+data.tic{file.scan.getLength} = [];
+
+if file.scan.getLength > 0
+
+    % Read time, total intensity values
+    for i = 1:file.scan.getLength
+    
+        % /msRun/scan/retentionTime
+        data.time{i} = value(file.scan, 'retentionTime', i-1);
+        
+        % /msRun/scan/totIonCurrent
+        data.tic{i} = value(file.scan, 'totIonCurrent', i-1);
+    end
+    
+    % Parse time values
+    expression = '[0-9].+[0-9]';
+    units = data.time{1}(end);
+    
+    % Convert time values to doubles
+    data.time = cellfun(@(x) str2double(regexp(x, expression, 'match')), data.time)';
+    
+    % Convert time values to minutes
+    if strcmpi(units, 's')
+        data.time = data.time / 60;
+    elseif strcmpi(units, 'h')
+        data.time = data.time * 60;
+    end
+    
+    % Convert total intensity values to doubles
+    data.tic = cellfun(@(x) str2double(x), data.tic)';
+    
+    % Replace NaN values with 0
+    data.time(isnan(data.time)) = 0;
+    data.tic(isnan(data.tic)) = 0;
+end
 end
 
 
